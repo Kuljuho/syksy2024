aea8128ddd6bf38afdc217d6a00f41f67c9c10ec
Juho Kultala <128252330+Kuljuho@users.noreply.github.com>
2024-09-09 19:37:43 +0300

    Modal toimii

---
+++ Web ja hybriditeknologiat/Modal/App.js
@@ -0,0 +1,71 @@
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, View, Modal } from "react-native";
+import { useState } from "react";
+
+export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={() => setModalVisible(true)}>
+        <Text>Show modal message</Text>
+      </Pressable>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text>This is modal...</Text>
+            <Pressable
+              style={styles.button}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    backgroundColor: "#2196F3",
+    marginTop: 15,
+  },
+});
